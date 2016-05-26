5d0e4e251de6678c59870c68c87d4ce5cc20c473
Colin Reeder <vpzomtrrfrt@gmail.com>
2016-05-26 08:44:04 -0600

    Initial commit

---
+++ src/main/scala/net/reederhome/colin/mods/exparvis/ExParvis.scala
@@ -0,0 +1,90 @@
+package net.reederhome.colin.mods.exparvis
+
+import com.google.common.base.Predicate
+import net.minecraft.block.IGrowable
+import net.minecraft.entity.item.EntityItem
+import net.minecraft.init.{Blocks, Items}
+import net.minecraft.item.{Item, ItemStack}
+import net.minecraft.util.math.BlockPos
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent
+
+import scala.collection.JavaConverters._
+import scala.util.Random
+
+@Mod(name = ExParvis.NAME, modid = ExParvis.MODID, version = ExParvis.VERSION, modLanguage = "scala")
+object ExParvis {
+  final val NAME = "Ex Parvis"
+  final val MODID = "exparvis"
+  final val VERSION = "${version}"
+
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent): Unit = {
+    MinecraftForge.EVENT_BUS.register(this)
+  }
+
+  @SubscribeEvent
+  def onLivingUpdate(event: LivingUpdateEvent): Unit = {
+    val entity = event.getEntityLiving
+    val world = entity.worldObj
+    val data = entity.getEntityData
+    val lastSneakKey = "LastTickSneaking"
+    val range = 1
+    if (data.hasKey(lastSneakKey) && data.getBoolean(lastSneakKey) != entity.isSneaking && Math.random() < 0.02) {
+      val x = event.getEntity.posX + Random.nextInt(range * 2 + 1) - range
+      val y = event.getEntity.posY
+      val z = event.getEntity.posZ + Random.nextInt(range * 2 + 1) - range
+      val pos = new BlockPos(x, y, z)
+      val state = world.getBlockState(pos)
+      val block = state.getBlock
+      if (block.isInstanceOf[IGrowable]) {
+        val growable = block.asInstanceOf[IGrowable]
+        if (!world.isRemote && growable.canGrow(world, pos, state, world.isRemote) && growable.canUseBonemeal(world, new java.util.Random(), pos, state)) {
+          growable.grow(world, new java.util.Random(), pos, state)
+        }
+      }
+    }
+    data.setBoolean(lastSneakKey, entity.isSneaking)
+  }
+
+  @SubscribeEvent
+  def onTick(event: WorldTickEvent): Unit = {
+    val numToCompost = 8
+    val timeToCompost = 200
+
+    val items: List[EntityItem] = event.world.getEntities[EntityItem](classOf[EntityItem], new Predicate[EntityItem] {
+      override def apply(input: EntityItem): Boolean = {
+        input.getAge >= timeToCompost &&
+          ((input.getEntityItem.getItem == Item.getItemFromBlock(Blocks.sapling) && input.getEntityItem.stackSize >= numToCompost) ||
+            input.getEntityItem.getItem == Items.bucket && event.world.isRainingAt(new BlockPos(input.posX, input.posY, input.posZ)))
+      }
+    }).asScala.toList
+    for (item: EntityItem <- items) {
+      val stack = item.getEntityItem
+      var result: ItemStack = null
+      if (stack.getItem == Items.bucket) {
+        stack.stackSize -= 1
+        result = new ItemStack(Items.water_bucket)
+      }
+      else {
+        var composted = 0
+        while (stack.stackSize >= numToCompost) {
+          stack.stackSize -= numToCompost
+          composted += 1
+        }
+        result = new ItemStack(Blocks.dirt, composted)
+      }
+      val newItem = new EntityItem(event.world, item.posX, item.posY, item.posZ)
+      newItem.setEntityItemStack(result)
+      event.world.spawnEntityInWorld(newItem)
+      if (stack.stackSize == 0) {
+        item.setDead()
+      }
+    }
+  }
+}
